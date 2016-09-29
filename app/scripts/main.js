1c39237ae31682261a4863ae0a18ad9d7f46c53d
Akshit Anand <f2013536@goa.bits-pilani.ac.in>
2016-09-29 22:49:17 +0530

    content addition and optimization

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,42 +1,12 @@
 $(document).ready(function(){
     
     //alert("hello");
-    
-	$("#goTop").on("click", function() {
-		$("body").animate({scrollTop: 0}, 750);
-	});
 
-	$("#section-skills").click(function() {
-	    $('html, body').animate({
-	        scrollTop: $("#skills").offset().top
-	    }, 750);
-	});
+	$('.scrollspy').scrollSpy();
 
-	$("#section-works").click(function() {
-	    $('html, body').animate({
-	        scrollTop: $("#works").offset().top
-	    }, 750);
-	});
-
-	$("#section-blog").click(function() {
-	    $('html, body').animate({
-	        scrollTop: $("#blog").offset().top
-	    }, 750);
-	});
-
-	$("#section-education").click(function() {
-	    $('html, body').animate({
-	        scrollTop: $("#education").offset().top
-	    }, 750);
-	});
-
-	$(".section-contact").click(function() {
-	    $('html, body').animate({
-	        scrollTop: $("#contact").offset().top
-	    }, 1000);
-	});
-
-
+    $("#goTop").on('click', function() {
+    	$('html, body').animate({scrollTop: 0}, 'slow');
+    });
 
 });
 

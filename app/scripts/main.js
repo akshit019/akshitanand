e205b7df0f19a70a85babb487d27c280a3ead7c2
Akshit Anand <f2013536@goa.bits-pilani.ac.in>
2016-10-02 20:46:17 +0530

    media query optimization

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,11 +2,21 @@ $(document).ready(function(){
     
     //alert("hello");
 
+    // preloader
+    $(window).on('load', function() { // makes sure the whole site is loaded 
+        //$('#status').fadeOut(); // will first fade out the loading animation 
+        $('#preloader').delay(1000).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+        $('body').delay(1000).css({'overflow':'visible'});
+    });
+
 	$('.scrollspy').scrollSpy();
 
     $("#goTop").on('click', function() {
     	$('html, body').animate({scrollTop: 0}, 'slow');
     });
+
+    //sidenav button
+    $(".button-collapse").sideNav();
 
 });
 
